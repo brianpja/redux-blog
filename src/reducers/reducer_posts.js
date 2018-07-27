727f8ccd8e333e9759340a12ce37a409d244b93a
Brian James <brian.james@slalom.com>
2018-07-27 14:39:56 -0700

    added delete post action to reducers

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import { FETCH_POSTS, FETCH_POST } from '../actions/index';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions/index';
 
 
 export default function(state = {}, action) {
